4d094533dc07792cf3f6388509f2daaa3ef21c24
Fritz1414213562 <fritz1414213562@gmail.com>
2020-05-03 11:45:48 +0900

    feat: include path for dcd_writer class

--- filestream/dcd.hpp
+++ filestream/dcd.hpp
@@ -2,6 +2,10 @@
 #define QAHWA_DCD_FILESTREAM_HPP
 #include<Qahwa/filestream/dcd/dcd_parser.hpp>
 #include<Qahwa/filestream/dcd/dcd_reader.hpp>
-#include<Qahwa/filestream/dcd/snapshot.hpp>
+#include<Qahwa/filestream/dcd/dcd_writer.hpp>
+#include<Qahwa/filestream/dcd/dcd_data_type/snapshot.hpp>
+#include<Qahwa/filestream/dcd/dcd_data_type/dcd_1stheader.hpp>
+#include<Qahwa/filestream/dcd/dcd_data_type/dcd_2ndheader.hpp>
+#include<Qahwa/filestream/dcd/dcd_data_type/dcd_3rdheader.hpp>
 
 #endif /* QAHWA_DCD_FILESTREAM_HPP */
